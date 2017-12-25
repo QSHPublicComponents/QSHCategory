d80bd4dac217dbb54e5cede81e6469c7581fbda8
NoryCao <2252055382@qq.com>
2017-12-25 11:38:39 +0800

    first commit

---
+++ QSHCategory/QSHCategory.h
@@ -0,0 +1,19 @@
+//
+//  QSHCategory.h
+//  QSHCategory
+//
+//  Created by caonongyun on 2017/12/25.
+//  Copyright © 2017年 QSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QSHCategory.
+FOUNDATION_EXPORT double QSHCategoryVersionNumber;
+
+//! Project version string for QSHCategory.
+FOUNDATION_EXPORT const unsigned char QSHCategoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QSHCategory/PublicHeader.h>
+
+
